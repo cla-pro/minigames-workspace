59bfd053d48dec50a3f23ae93443143b857cad49
u213429 <cedric.lavanchy@sbb.ch>
2023-11-29 11:29:42 +0100

    Change the board to an image and the days are covering it. As soon as a game is completed, it disappear from the board to reveal the image

--- projects/minigames-advent/src/app/advent-service-initiator.service.ts
+++ projects/minigames-advent/src/app/advent-service-initiator.service.ts
@@ -9,49 +9,14 @@ import { MinigameParkingjamCar } from 'projects/minigame-parkingjam/src/lib/shar
 })
 export class AdventServiceInitiatorService {
 
-  constructor(private service: AdventScenarioService) { }
+  constructor(private service: AdventScenarioService) {
+    let stored = localStorage.getItem('scenario-stored');
+    if (stored !== 'true') {
+      this.initiateScenarios();
+    }
+  }
 
   initiateScenarios(): void {
-    let walls = [
-      // left wall
-      this.wall(1, true, 0, 1, 0, 0),
-      this.wall(2, true, 1, 2, 0, 0),
-      this.wall(3, true, 2, 3, 0, 0),
-      this.wall(4, true, 3, 4, 0, 0),
-      this.wall(5, true, 4, 5, 0, 0),
-      this.wall(6, true, 5, 6, 0, 0),
-      this.wall(7, true, 6, 7, 0, 0),
-      // right wall
-      this.wall(8, true, 0, 1, 5, 5),
-      this.wall(9, true, 1, 2, 5, 5),
-      this.wall(10, true, 2, 3, 5, 5),
-      this.wall(11, true, 3, 4, 5, 5),
-      this.wall(12, true, 4, 5, 5, 5),
-      this.wall(13, true, 6, 7, 5, 5),
-      // upper wall
-      this.wall(14, false, 0, 0, 0, 1),
-      this.wall(15, false, 0, 0, 1, 2),
-      this.wall(16, false, 0, 0, 2, 3),
-      this.wall(17, false, 0, 0, 3, 4),
-      this.wall(18, false, 0, 0, 4, 5),
-      // lower wall
-      this.wall(19, false, 7, 7, 0, 1),
-      this.wall(20, false, 7, 7, 1, 2),
-      this.wall(21, false, 7, 7, 2, 3),
-      this.wall(22, false, 7, 7, 3, 4),
-      this.wall(23, false, 7, 7, 4, 5),
-      // inner walls
-      this.wall(24, false, 1, 1, 1, 2),
-      this.wall(25, false, 3, 3, 4, 5)
-    ];
-
-    let cars = [
-      this.car(1, 5, 0, 2, false, '1x2-red-car-left-right.png', true),
-      this.car(2, 2, 1, 3, true, '3x1-blue-truck-top-down.png', false),
-      this.car(3, 3, 2, 3, false, '1x3-blue-truck-left-right.png', false),
-      this.car(4, 5, 4, 2, true, '2x1-red-car-top-down.png', false)
-    ];
-    
     localStorage.clear();
 
     let firstScenario = new AdventScenarioWordle("2023-12-01", "GLACE");
@@ -80,8 +45,9 @@ export class AdventServiceInitiatorService {
       new AdventScenarioWordle("2023-12-21", "VACHE"),
       //new AdventScenarioMemory("2023-12-22"),
       this.parkingjamScenario5(),
-      new AdventScenarioPuzzle("2023-12-24")
+      new AdventScenarioWordle("2023-12-24", "RUINE")
     ]);
+    localStorage.setItem('scenario-stored', 'true');
   }
 
   private wall(id: number, vertical: boolean, lineFrom: number, lineTo: number, columnFrom: number, columnTo: number) {
